d8b2c1f0bb08968b07f03ad0a6e9e9e330b6907d
Riccardo Crippa <therickys93@gmail.com>
2016-07-14 18:20:20 +0200

    corretto un bug sul file onlytest.sh

--- onlytest.sh
+++ onlytest.sh
@@ -4,8 +4,10 @@ docker-compose -f docker-compose.test.yml -p ci build
 docker-compose -f docker-compose.test.yml -p ci up -d
 docker logs -f ci_sut_1
 docker wait ci_sut_1
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
     printf "All Tests are passing\n"
+else
+    printf "Some Tests are not passing\n"
 fi
 docker-compose -f docker-compose.test.yml -p ci kill
 docker rm ci_sut_1
